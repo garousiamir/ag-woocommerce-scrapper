bfea733e49c90ec38182189ee1c2f78669badf43
Amirhossein Garousi <59143499+garousiamir@users.noreply.github.com>
2023-11-17 15:24:52 +0330

    added html parser library and initial init

---
+++ ag-woocommerce-scrapper.php
@@ -0,0 +1,21 @@
+<?php
+/*
+    Plugin Name: ag woocommerce scrapper
+    Plugin URI: https://agarousi.ir
+    Version: 1.0
+    description: Simple Woocommerce Scrapper
+    Author: Amirhossein Garousi
+    Author URI: https://agarousi.ir
+*/
+
+
+//classes-loader
+require 'classes-loader.php';
+
+//load from composer
+// require 'vendor/autoload.php';
+// use PHPHtmlParser\Dom;
+
+// $dom = new Dom;
+// $dom->loadFromUrl('https://www.trendyol.com/bershka/fitilli-kisa-kollu-t-shirt-p-382836459?boutiqueId=618519&merchantId=104961');
+// echo $dom->find('h1.pr-new-br')[0]->innerHtml;
---
+++ classes-loader.php
@@ -0,0 +1,10 @@
+<?php
+//automatically require classes
+spl_autoload_register( function($classname) {
+    $class      = str_replace( '\\', DIRECTORY_SEPARATOR, str_replace( '_', '-', strtolower($classname) ) );
+    $classes    = dirname(__FILE__) .  DIRECTORY_SEPARATOR . 'classes' . DIRECTORY_SEPARATOR . $class . '.php'; 
+    
+    if( file_exists($classes) ) {
+        require_once( $classes );
+    }    
+} );
