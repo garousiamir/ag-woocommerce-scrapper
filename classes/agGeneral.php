61c5743389ff7d38c3552baaa3df343af85b0331
Amirhossein Garousi <59143499+garousiamir@users.noreply.github.com>
2023-11-19 17:54:16 +0330

    added some options

--- classes/agGeneral.php
+++ classes/agGeneral.php
@@ -1,10 +1,6 @@
 <?php
 class agGeneral {
     public function __construct() {
-        add_action('admin_enqueue_scripts', array($this, 'ag_admin_styles'));
     }
 
-    public function ag_admin_styles() {
-        wp_enqueue_style('ag-admin-style', plugin_dir_url(dirname(__FILE__)) . 'assets/ag-admin-style.css');
-    }
 }
--- classes/agOptions.php
+++ classes/agOptions.php
@@ -18,9 +18,14 @@ class agOptions {
 
             $sections = array(
                 array(
-                    'id'    => 'one_tab', // Set a unique slug-like ID
+                    'id'    => 'tab_one', // Set a unique slug-like ID
                     'title' => 'تنظیمات عمومی',
                     'fields' => $this->get_section_one_fields(),
+                ),
+                array(
+                    'id'    => 'tab_two', // Set a unique slug-like ID
+                    'title' => 'تنظیمات قیمت',
+                    'fields' => $this->get_section_two_fields(),
                 ),
              
             );
@@ -34,105 +39,51 @@ class agOptions {
     private function get_section_one_fields() {
         return array(
                 array(
-                'id'           => 'bglogin',
-                'type'         => 'upload',
-                'title'        => 'آپلود بک گراند صفحه لاگین وردپرس',
-                'library'      => 'image',
-                'placeholder'  => 'http://',
-                'button_title' => 'آپلود تصویر',
-                'remove_title' => 'حذف تصویر',
-                'default' => 'https://s21.uupload.ir/files/garousiamir/autumn-season-leafs-plant-scene-generative-ai.jpg',
-                ),
-                array(
-                'id'           => 'kaveh_logo_up',
-                'type'         => 'upload',
-                'title'        => 'آپلود لوگو',
-                'library'      => 'image',
-                'placeholder'  => 'http://',
-                'button_title' => 'آپلود تصویر',
-                'remove_title' => 'حذف تصویر',
-                'default' => 'https://kaveh.moeinwp.com/1/wp-content/uploads/2022/10/demo1.svg',
-                ),
-                array(
-                'id'      => 'kaveh_preload_switch',
-                'type'    => 'switcher',
-                'title'   => 'پیش بارگزاری صفحات',
-                'text_on'    => 'فعال',
-                'text_off'   => 'غیرفعال',
-                'default' => false
-                ),
-            
-        
-                array(
-                'id'         => 'search_kaveh_tasavir',
+                'id'         => 'auto_update_products',
                 'type'       => 'switcher',
-                'title'      => 'بنر های تبلیغاتی در بخش سرچ',
+                'title'      => 'بروزرسانی محصولات',
                 'default'    => false
                 ),
-        
                 array(
-                'id'        => 'search-pic-repeater',
-                'type'      => 'repeater',
-                'title'     => 'تصاویر اسلایدر باکس سرچ',
-                'dependency' => array( 'search_kaveh_tasavir', '==', '1' ),
-                'fields'    => array(
-        
-                    array(
-                        'id'           => 'searc-pic',
-                        'type'         => 'upload',
-                        'title'        => 'تصویر اسلایدر',
-                        'library'      => 'image',
-                        'placeholder'  => 'http://',
-                        'button_title' => 'آپلود تصویر',
-                        'remove_title' => 'حذف تصویر',
+                    'id'    => 'update_products_min_time',
+                    'type'  => 'number',
+                    'title' => 'کم ترین فاصله زمانی بروزرسانی محصولات',
+                    'dependency' => array( 'auto_update_products', '==', '1' ),
+
+                  ),
+                  array(
+                    'id'    => 'update_products_max_time',
+                    'type'  => 'number',
+                    'title' => 'بیشترین فاصله زمان بروزرسانی محصولات',
+                    'dependency' => array( 'auto_update_products', '==', '1' ),
+                  ),
+                  array(
+                    'id'          => 'scrapper_default_category',
+                    'type'        => 'select',
+                    'multiple'            => true,
+                    'title'       => 'دسته بندی پیش فرض محصولات',
+                    'placeholder' => 'Select a category',
+                    'options'     => 'categories',
+                    'query_args'  => array(
+                      'taxonomy'  => 'product_cat',
                     ),
-                    array(
-                        'id'           => 'searc-pic-url',
-                        'type'         => 'link',
-                        'title'        => 'لینک آیکون دوم',
-                        'add_title'    => 'اضافه کردن لینک',
-                        'edit_title'   => 'ویرایش لینک',
-                        'remove_title' => 'حذف لینک',
-                    ),
-        
-        
-                ),
-                'default'   => array(
-                    array(
-                    'searc-pic' => 'https://kaveh.moeinwp.com/4/wp-content/uploads/2023/02/slider.png',
-                    ),
-                    array(
-                    'searc-pic' => 'https://kaveh.moeinwp.com/4/wp-content/uploads/2023/02/cloths3.jpg',
-                    ),
-                    array(
-                    'searc-pic' => 'https://kaveh.moeinwp.com/4/wp-content/uploads/2023/02/cloths2.jpg',
-                    ),
-                )
-                ),
-        
-                array(
-                'id'           => 'aj-search',
-                'type'         => 'upload',
-                'title'        => 'تصویر کوچک اول سرچ',
-                'library'      => 'image',
-                'placeholder'  => 'http://',
-                'button_title' => 'آپلود تصویر',
-                'remove_title' => 'حذف تصویر',
-                'dependency' => array( 'search_kaveh_tasavir', '==', '1' ),
-                'default' => 'https://kaveh.moeinwp.com/1/wp-content/uploads/2022/08/banner-03.png',
-        
-                ),
-                array(
-                'id'           => 'aj-search2',
-                'type'         => 'upload',
-                'title'        => 'تصویر کوچک اول دوم',
-                'library'      => 'image',
-                'placeholder'  => 'http://',
-                'button_title' => 'آپلود تصویر',
-                'remove_title' => 'حذف تصویر',
-                'dependency' => array( 'search_kaveh_tasavir', '==', '1' ),
-                'default' => 'https://kaveh.moeinwp.com/1/wp-content/uploads/2022/08/banner-02.png',
-                ),
+                  ),
+        );
+    }
+    private function get_section_two_fields() {
+        return array(
+            array(
+                'id'    => 'office_price',
+                'type'  => 'number',
+                'title' => 'هزینه دفتر',
+                'dependency' => array( 'auto_update_products', '==', '1' ),
+              ),
+              array(
+                'id'    => 'cargo_price',
+                'type'  => 'number',
+                'title' => 'هزینه کارگو و حمل',
+                'dependency' => array( 'auto_update_products', '==', '1' ),
+              ),
         );
     }
 
